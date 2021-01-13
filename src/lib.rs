d46a44c03d848ef12f9f2cf40fa5cec44d49f352
hices <shawnhice@gmail.com>
2021-01-12 16:02:16 -0800

    Forced it to work with git version of wasm-pack

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 #[wasm_bindgen]
 pub fn add(n1: i32, n2: i32) -> i32 {
     n1 + n2
+}
+
+// Called by our JS entry point to run the example.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
 }
